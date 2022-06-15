31214d63c0db0194999e09a1b2808e5a50155356
sddsergio <destefanosergio@hotmail.com>
2022-06-15 14:36:20 -0300

    app.js

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,10 @@ const app = express()
 
 app.get("/", (req, res) => {
 
-    res.send("<h1>Hola mundo!!!</h1>")
-    
+    /*res.send("<h1>Hola mundo!!!</h1>")*/
 
+    document.write("<h2>Hola</h2>")
+    
     console.log("peticion recibida")
 
 });
